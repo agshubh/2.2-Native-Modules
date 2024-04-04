9d0b3309569c214e94651e27665cc73d262fb704
Shubh Agarwal <f20212770@goa.bits-pilani.ac.in>
2024-04-05 04:42:40 +0530

    hello

---
+++ index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+fs.writeFile("message.txt","Hello World!!", (err) => {
+    if(err) throw err;
+    console.log("File is created successfully.");
+});
+fs.readFile("message.txt", "utf8", (err, data) => {
+    if(err) throw err;
+    console.log("The data is: "+data);
+});
